c368fcf9295486cb5eede7d46a8716f41df32d93
Felipe Almeida <felipesba@gmail.com>
2019-04-02 23:04:36 -0300

    First Controller and Razor Pages

--- SalesWebMvC/Controllers/HomeController.cs
+++ SalesWebMvC/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace SalesWebMvC.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
