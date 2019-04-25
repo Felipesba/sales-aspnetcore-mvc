83476f5ea622d8f014c34656db1ae8a41e45ec4e
Felipe Almeida <felipesba@gmail.com>
2019-04-24 22:39:12 -0300

    FreignKey not Null

--- SalesWebMvC/Migrations/20190425013031_DepartmentForeignKey.Designer.cs
+++ SalesWebMvC/Migrations/20190425013031_DepartmentForeignKey.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMvC.Models;
 
 namespace SalesWebMvC.Migrations
 {
     [DbContext(typeof(SalesWebMvCContext))]
-    partial class SalesWebMvCContextModelSnapshot : ModelSnapshot
+    [Migration("20190425013031_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
