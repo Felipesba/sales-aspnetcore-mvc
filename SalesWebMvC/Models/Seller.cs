ea6be36aed2314dc6effa15da405d3d6542b910c
Felipe Almeida <felipesba@gmail.com>
2019-05-20 21:33:19 -0300

    Field Validation

--- SalesWebMvC/Models/Seller.cs
+++ SalesWebMvC/Models/Seller.cs
@@ -9,16 +9,24 @@ namespace SalesWebMvC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} - campo requerido")]
+        [StringLength(60,MinimumLength =3, ErrorMessage ="{0} tamanho deve ser entre {2} e {1} caracteres")]
         public String Name { get; set; }
 
+        [Required(ErrorMessage = "{0} - campo requerido")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Entre com um {0} válido")]
         public String email { get; set; }
 
+        [Required(ErrorMessage = "{0} - campo requerido")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Birthdate { get; set; }
 
+        [Required(ErrorMessage = "{0} - campo requerido")]
+        [Range(1000,50000, ErrorMessage ="O {0} deve ser de {1} a {2}")]
         [Display(Name = "Base Salary")]
         [DataType(DataType.Currency)]
         //[DisplayFormat(DataFormatString ="{0:F2}")]
